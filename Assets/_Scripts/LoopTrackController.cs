5327a3a12d9b1eae3ad23ac86b190aec034bb3d5
STUDIOBOOK\SeanF <sean.feeser@gmail.com>
2025-01-25 09:59:26 -0500

    Loop Manager and debug audio

---
+++ Assets/_Scripts/LoopTrackController.cs
@@ -0,0 +1,78 @@
+using Oculus.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoopTrackController : MonoBehaviour
+{
+
+    public PokeInteractable recordButton, playButton;
+
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoopManager.OnStartPlayback += OnStartPlayback;
+        LoopManager.OnStopPlayback += OnStopPlayback;
+        LoopManager.OnStartRecording += OnStartRecording;
+        LoopManager.OnStopRecording += OnStopRecording;
+
+        Setup();
+    }
+
+    private void OnDestroy()
+    {
+        LoopManager.OnStartPlayback -= OnStartPlayback;
+        LoopManager.OnStopPlayback -= OnStopPlayback;
+        LoopManager.OnStartRecording -= OnStartRecording;
+        LoopManager.OnStopRecording -= OnStopRecording;
+    }
+
+    void Setup()
+    {
+        playButton.gameObject.SetActive(false);
+    }
+
+    private void OnStartPlayback(int obj)
+    {
+        if (obj != index)
+            return;
+        playButton.GetComponentInChildren<TMP_Text>().text = "Stop";
+    }
+
+    private void OnStopPlayback(int obj)
+    {
+        if (obj != index)
+            return;
+        playButton.GetComponentInChildren<TMP_Text>().text = "Play";
+    }
+
+    private void OnStartRecording(int obj)
+    {
+        if(obj == index)
+        {
+            recordButton.GetComponentInChildren<TMP_Text>().text = "[]";
+            playButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            recordButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnStopRecording(int obj)
+    {
+        if (obj == index)
+        {
+            recordButton.GetComponentInChildren<TMP_Text>().text = "O";
+            playButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            recordButton.gameObject.SetActive(true);
+        }
+    }
+}
