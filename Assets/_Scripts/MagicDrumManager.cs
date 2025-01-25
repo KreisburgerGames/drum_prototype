4a498666d2645f37e52cdfaa537240c6e27cb3ee
STUDIOBOOK\SeanF <sean.feeser@gmail.com>
2025-01-25 12:53:18 -0500

    Error handling for weird room setups

--- Assets/_Scripts/MagicDrumManager.cs
+++ Assets/_Scripts/MagicDrumManager.cs
@@ -27,9 +27,15 @@ public class MagicDrumManager : MonoBehaviour
         MRUKAnchor[] anchors = FindObjectsOfType<MRUKAnchor>();
         foreach (MRUKAnchor anchor in anchors)
         {
+
+            Collider collider = anchor.GetComponentInChildren<Collider>();
+
+            if(collider == null)
+                continue;
+
             string label = anchor.Label.ToString();
             Debug.Log("Generate audio for label " +  label);
-            SoundGenerator.GenerateSound(label, (result)=>anchor.GetComponentInChildren<Collider>().gameObject.AddComponent<SoundCollider>().Setup(label, result));
+            SoundGenerator.GenerateSound(label, (result)=>collider.gameObject.AddComponent<SoundCollider>().Setup(label, result));
 
             GameObject HOP = Instantiate(hittableObjectParticlePrefab);
             HOP.transform.position = anchor.GetComponentInChildren<MeshFilter>().gameObject.transform.position;
